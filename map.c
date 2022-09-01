5e7eee389355b3434dc2d92f51d3f2c094d4b8ea
Safon686 <safon686@gmail.com>
2022-09-02 03:18:23 +0700

    v2.0 add control

---
+++ map.c
@@ -0,0 +1,60 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+	srand(time(NULL));
+	char map[10][21];
+	char direction;
+	char start;
+	int x=5,y=5;
+	int ox,oy;
+	int q=2,a=2;
+	printf("\n W-up, S-down, A-left, D-right, E-exit.\n After the command, press Enter.\n");
+	printf(" Enter ''G'' to start\n\n");
+	scanf("%c", &start);
+	if ((start=='G')||(start=='g')) {
+	do
+	{
+        sprintf(map[0], "####################");
+        
+	for (int i=1;i<9;i++) {
+            sprintf(map[i], "#                  #");
+        }
+        sprintf(map[9], "####################");
+        
+	map[y][x]='@';
+	map[q][a]='*';
+
+	system("clear");
+
+	for (int i=0; i<10; i++) {
+             printf("%s\n", map[i]);
+        }
+
+	direction = getchar();
+		
+	ox=x;
+	oy=y;
+
+	if (direction == 'w') y--;
+	if (direction == 's') y++;
+	if (direction == 'd') x++;
+	if (direction == 'a') x--;
+	if (map[y][x] == '#') 
+	{
+	x=ox;
+	y=oy;
+	}
+	if ((x==a)&&(y==q)) {
+
+	a=rand()*1.0 / RAND_MAX * 18+1;
+	q=rand()*1.0 / RAND_MAX * 8+1;
+	}
+	}
+	while (direction !='e');
+	}
+
+	return 0;
+}
